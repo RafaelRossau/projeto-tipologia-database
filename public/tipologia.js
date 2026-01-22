a3bc10248cb04f120e19b7966dfc07e4ce2405d0
Rafael Rossau <rafaelrossau@gmail.com>
2026-01-22 17:57:01 -0300

    Arrumando o adicionador de personagens e botando uma verificação

--- public/tipologia.js
+++ public/tipologia.js
@@ -1,4 +1,4 @@
-//---------------------------------------------------------->ADIÇÃO DE PERSONAGEM<----------------------------------------------------------------
+//----------------------------------------------------------> ADIÇÃO DE PERSONAGEM <----------------------------------------------------------------
 
 function adicionarPersonagem(){
     const pegarMarcados = (idContainer) => {
@@ -10,33 +10,81 @@ function adicionarPersonagem(){
     };
 const nome_adicao = document.getElementById("nome_adicao").value;
 const foto_adicao = document.getElementById("foto_adicao").value;
-
-const MBTI = document.getElementById("MBTI_escolha");
 const marcados_MBTI = pegarMarcados("MBTI_escolha")
-
-const Anagrama = document.getElementById("anagrama_escolha");
 const marcados_anagrama =pegarMarcados("Anagrama_escolha");
-
-const Subtipo = document.getElementById("subtipo_escolha");
 const marcados_subtipo = pegarMarcados("subtipo_escolha")
-
 const tritipo = document.getElementById("tritipo_escolha").value
-
-const Temperamento = document.getElementById("temperamento_escolha");
 const marcados_temperamento = pegarMarcados("temperamento_escolha");
-
-const Big_Five = document.getElementById("big_five_escolha");
 const marcados_Big_Five = pegarMarcados("big_five_escolha");
-
-const Alinhamento = document.getElementById("alinhamento_escolha");
 const marcados_alinhamento = pegarMarcados("alinhamento_escolha");
-console.log(marcados_MBTI)
 
+let head = false
+let guts = false
+let heart = false
+let triade = false
+
+if(tritipo.includes("2") || tritipo.includes("3") || tritipo.includes("4")){
+    heart = true
+}
+else{
+    heart = false
+}
+if(tritipo.includes("5") || tritipo.includes("6") || tritipo.includes("7")){
+head = true
+}
+else{
+head = false
+}
+if(tritipo.includes("8") || tritipo.includes("9") || tritipo.includes("1")){
+    guts = true
+}
+else{
+    guts = false
+}
+if(heart == true && head == true && guts == true){
+    triade = true
+}
+else{
+    triade = false
+}
+console.log(marcados_MBTI)
+console.log(marcados_anagrama)
+console.log(tritipo)
+if(nome_adicao.length == 0 || nome_adicao.length > 50){
+    alert("O nome não pode ser vazio ou ultrapassar o limite de 50 caracteres.")
+}
+else if(foto_adicao === ""){
+alert("Selecione uma foto.")
+}
+else if(marcados_MBTI.length !== 1){
+alert("Selecione apenas um MBTI.")
+}
+else if(marcados_anagrama.length !== 1){
+    alert("Selecione apenas um anagrama.")
+}
+else if(marcados_subtipo.length !== 1){
+    alert("Selecione apenas um subtipo")
+}
+else if(tritipo.length !== 3 || triade == false){
+    alert("Digite um tritipo existente!(OBS: o tritipo é composto por 3 anagramas e cada anagrama vem de um centro diferente, tendo que ter um do intestino, coração e mente.)")
+}
+else if(marcados_temperamento.length !== 1){
+    alert("Selecione apenas um temperamento")
+}
+else if(marcados_Big_Five.length !== 1){
+    alert("Selecione apenas um Big Five.")
+}
+else if(marcados_alinhamento.length !== 1){
+    alert("Selecione apenas um alinhamento moral.")
+}
+else{
+    alert("Enviado com sucesso!")
 fetch("/Personagens", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({nome_adicao, foto_adicao,marcados_MBTI, marcados_anagrama, marcados_subtipo, tritipo, marcados_temperamento, marcados_Big_Five, marcados_alinhamento})
     })
-
+}
     
 }
+
--- tipologiaDataBase.sql
+++ tipologiaDataBase.sql
@@ -2,14 +2,15 @@ create database Tipologia;
 use Tipologia;
 
 create table Personagens(
-id int primary key,
+id int primary key auto_increment,
 foto varchar(100) not null,
 nome varchar(50) not null,
 mbti char(4) not null,
 anagrama varchar(50) not null,
 subtipo varchar(20) not null,
 tritipo char(3) not null,
-temperamento varchar(20) not null,
+temperamento varchar(21) not null,
 big_five char(5) not null,
 alinhamento varchar(20) not null
 );
+drop table Personagens;
